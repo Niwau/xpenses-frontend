eee8ec6abb3c3a41b8aaf16f92933f8b83ac8958
Guilherme Ribeiro <85759155+Niwau@users.noreply.github.com>
2024-02-05 19:09:30 -0300

    refactor: railway to render

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://xpenses-backend-production.up.railway.app'
+  baseURL: 'https://xpenses-backend-uktd.onrender.com'
 })
